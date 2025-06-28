8bc539b418728d3e317d04615fde08d5ec0d5eb8
胖即是胖 <amaipanda@qq.com>
2025-06-28 17:43:51 +0800

    欧列格17

--- pkg/panda1/skills/pang_tanta.lua
+++ pkg/panda1/skills/pang_tanta.lua
@@ -10,8 +10,6 @@ skill:addEffect("viewas", {
   handly_pile = true,
   card_filter = function(self, player, to_select, selected)
     if #selected == 3 then return false end
-    local record = Self:getTableMark("@hs__luanji-turn")
-    return not table.contains(record, Fk:getCardById(to_select):getSuitString(true))
   end,
   view_as = function(self, player, cards)
     if #cards ~= 3 then
@@ -22,20 +20,11 @@ skill:addEffect("viewas", {
     card:addSubcards(cards)
     return card
   end,
-  before_use = function(self, player, use)
-    local room = player.room
-    local record = player:getTableMark("@hs__luanji-turn")
-    local cards = use.card.subcards
-    for _, cid in ipairs(cards) do
-      local suit = Fk:getCardById(cid):getSuitString(true)
-      if suit ~= "log_nosuit" then table.insertIfNeed(record, suit) end
-    end
-    room:setPlayerMark(player, "@hs__luanji-turn", record)
-  end
 })
-skill:addEffect("targetmod", {
-  bypass_times = function (self, player, skill, scope, card, to)
-    return card and table.contains(card.skillNames, skill.name)
+skill:addEffect("targetmod",{
+  frequency = Skill.Compulsory,
+  bypass_times = function(self, player, skill, scope, card)
+    return card and table.contains(card.skillNames, skill.name)    
   end,
 })
 
