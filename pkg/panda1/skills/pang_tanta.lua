61b67a33e1d36c903c84a0302b2dffbef13c80d3
胖即是胖 <amaipanda@qq.com>
2025-06-28 17:21:18 +0800

    欧列格15

--- pkg/panda1/skills/pang_tanta.lua
+++ pkg/panda1/skills/pang_tanta.lua
@@ -9,7 +9,8 @@ skill:addEffect("viewas", {
   pattern = "slash",
   handly_pile = true,
   card_filter = function(self, player, to_select, selected)
-    if #selected == 2 or Fk:currentRoom():getCardArea(to_select) ~= Player.Hand then return false end
+    if #selected == 2 then return false end
+    local record = Self:getTableMark("@hs__luanji-turn")
     return not table.contains(record, Fk:getCardById(to_select):getSuitString(true))
   end,
   view_as = function(self, player, cards)
@@ -20,12 +21,17 @@ skill:addEffect("viewas", {
     card.skillName = skill.name
     card:addSubcards(cards)
     return card
-  end
-})
-skill:addEffect("targetmod", {
-  bypass_times = function (self, player, skill, scope, card, to)
-    return card and table.contains(card.skillNames, skill.name)
   end,
+  before_use = function(self, player, use)
+    local room = player.room
+    local record = player:getTableMark("@hs__luanji-turn")
+    local cards = use.card.subcards
+    for _, cid in ipairs(cards) do
+      local suit = Fk:getCardById(cid):getSuitString(true)
+      if suit ~= "log_nosuit" then table.insertIfNeed(record, suit) end
+    end
+    room:setPlayerMark(player, "@hs__luanji-turn", record)
+  end
 })
 
 Fk:loadTranslationTable{["pang_tanta"] = "坦踏",
