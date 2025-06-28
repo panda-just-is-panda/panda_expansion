fe9b0ea807b96c14f316f310e81b29865d095b5a
胖即是胖 <amaipanda@qq.com>
2025-06-28 16:33:32 +0800

    欧列格6

--- pkg/panda1/skills/pang_tanta.lua
+++ pkg/panda1/skills/pang_tanta.lua
@@ -9,13 +9,13 @@ skill:addEffect("viewas", {
   pattern = "slash",
   handly_pile = true,
   card_filter = function(self, player, to_select, selected)
-    return #selected == 2
+    return #selected == 0 and Fk:getCardById(to_select).color == Card.Red
   end,
   view_as = function(self, player, cards)
-    if #cards ~= 3 then return end
+    if #cards ~= 1 then return end
     local c = Fk:cloneCard("slash")
     c.skillName = skill.name
-    c:addSubcard(cards[3])
+    c:addSubcard(cards[1])
     return c
   end,
 })
