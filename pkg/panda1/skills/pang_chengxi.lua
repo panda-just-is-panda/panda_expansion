ab0f579193673d3c0adccceb2fcfcea800d102de
胖即是胖 <amaipanda@qq.com>
2025-06-30 01:46:07 +0800

    梅兰妮20

--- pkg/panda1/skills/pang_chengxi.lua
+++ pkg/panda1/skills/pang_chengxi.lua
@@ -43,7 +43,7 @@ chengxi:addEffect(fk.TargetConfirming, {
     })
       room:moveCardTo(id2, Card.DiscardPile, nil, fk.ReasonPutIntoDiscardPile, chengxi.name, nil, true, player)
       if player.dead or player:isNude() or target.dead then return end
-      local cards2 = room:askToCards(player, {
+      local cards2 = room:askToChooseCards(player, {
         target = target,
         min_num = 1,
         max_num = 1,
