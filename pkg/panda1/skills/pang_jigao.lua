a9fccbeacb59233c7fa46ce53c9429daa6fad4de
胖即是胖 <amaipanda@qq.com>
2025-07-01 15:43:05 +0800

    husk25

--- pkg/panda1/skills/pang_jigao.lua
+++ pkg/panda1/skills/pang_jigao.lua
@@ -6,38 +6,40 @@ local jigao = fk.CreateSkill({
 jigao:addEffect(fk.Damage, {
   prompt = "#pang_jigao1",
   can_trigger = function(self, event, target, player, data)
-    return player:hasSkill(jigao.name) and target == player
+    return player:hasSkill(jigao.name) and target == data.to and not target.dead
   end,
   on_cost = function(self, event, target, player, data)
     local room = player.room
+    local to = data.to
     if room:askToSkillInvoke(player, {
       skill_name = jigao.name,
       prompt = "#pang_jigao1",
     }) then
       event:setCostData(self, {tos = {data.to}})
-      player:drawCards(2)
+      to:drawCards(2)
       return true
     end
     end,
     on_use = function(self, event, target, player, data)
     local room = player.room
-    local card = table.filter(player:getCardIds("he"), function(id)
+    local to = data.to
+    local card = table.filter(to:getCardIds("he"), function(id)
     local card_pick = Fk:getCardById(id)
-        return card_pick and card_pick.color == card_pick.Black and not player:prohibitDiscard(id)
+        return card_pick and card_pick.color == card_pick.Black and not to:prohibitDiscard(id)
         end)
     local choices = {"losehp"}
-    if not data.to:hasDelayedTrick("supply_shortage") 
-    and not table.contains(data.to.sealedSlots, data.to.JudgeSlot)
+    if not to:hasDelayedTrick("supply_shortage") 
+    and not table.contains(to.sealedSlots, to.JudgeSlot)
     and #card > 0
     then
       table.insert(choices, 2, "shortage")
     end
-    local choice = room:askToChoice(player, {
+    local choice = room:askToChoice(to, {
       choices = choices,
       skill_name = jigao.name,
     })
         if choice == "shortage" then
-            local to_select = room:askToCards(player, {
+            local to_select = room:askToCards(to, {
       min_num = 1,
       max_num = 1,
       include_equip = true,
@@ -50,7 +52,7 @@ jigao:addEffect(fk.Damage, {
             local card2 = Fk:cloneCard("supply_shortage")
       card2:addSubcards(to_select)
       if not player:prohibitUse(card2) and not player:isProhibited(player, card2) then
-    room:useVirtualCard("supply_shortage", card2, player, data.to, jigao.name)
+    room:useVirtualCard("supply_shortage", card2, to, to, jigao.name)
       end
     end
         else
@@ -62,7 +64,7 @@ jigao:addEffect(fk.Damage, {
 jigao:addEffect(fk.Damaged, {
   prompt = "#pang_jigao1",
   can_trigger = function(self, event, target, player, data)
-    return player:hasSkill(jigao.name) and target == player
+    return player:hasSkill(jigao.name) and target == player and not player.dead
   end,
   on_cost = function(self, event, target, player, data)
     local room = player.room
@@ -116,7 +118,7 @@ jigao:addEffect(fk.Damaged, {
 })
 
 Fk:loadTranslationTable{["pang_jigao"] = "饥槁",
-  [":pang_jigao"] = "当你造成或受到伤害后，你可以摸两张牌并选择一项：失去1点体力；将一张黑色牌作为【兵粮寸断】对受到伤害的角色使用。",
+  [":pang_jigao"] = "当你造成或受到伤害后，你可以令受到伤害的角色摸两张牌并选择一项：失去1点体力；将一张黑色牌作为【兵粮寸断】对自己使用。",
   ["#pang_jigao1"] = "你可以摸两张牌并选择失去体力或对其用【兵粮寸断】",
   ["losehp"] = "失去体力",
   ["shortage"] = "使用兵粮寸断",
