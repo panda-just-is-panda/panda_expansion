a77b275bd1e7a4023fe76a6134d664059210f347
胖即是胖 <amaipanda@qq.com>
2025-07-01 20:39:55 +0800

    分成两个包2

--- init.lua
+++ init.lua
@@ -1,10 +1,12 @@
 local prefix = "packages.panda.pkg."
 local panda1 = require (prefix.."panda1")
+local panda2 = require (prefix.."panda2")
 
 Fk:loadTranslationTable{
   ["panda"] = "胖胖身份",
 }
 
 return{
-   panda1
+   panda1,
+   panda2
 }
