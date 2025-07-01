99ec5bb89b89991a79e6a4b29c15c17bfcb2f38e
胖即是胖 <amaipanda@qq.com>
2025-07-01 23:17:53 +0800

    僵尸7

--- pkg/panda1/skills/pang_shifu.lua
+++ pkg/panda1/skills/pang_shifu.lua
@@ -17,7 +17,9 @@ shifu:addEffect(fk.TargetSpecified, {
     }) then
       room:loseHp(player, 1, shifu.name)
       room:loseHp(data.to, 1, shifu.name)
+      if not not data.to.dead then
       return true
+    end
     end
   end,
   on_use = function(self, event, target, player, data)
