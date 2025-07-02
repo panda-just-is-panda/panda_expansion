daac6be612ac39c4644f631f1769d45aaed1425e
胖即是胖 <amaipanda@qq.com>
2025-07-03 01:20:06 +0800

    pillager22

--- pkg/panda1/skills/pang_beilve.lua
+++ pkg/panda1/skills/pang_beilve.lua
@@ -24,7 +24,13 @@ on_cost = function(self, event, target, player, data)
     local room = player.room
     local num = 3 - player:getHandcardNum()
     if player.shield > 0 then
+      if player.room:askToSkillInvoke(player, {
+      skill_name = beilve.name,
+      prompt = "#beilve2",
+    }) then
+      room:changeShield(player, -1)
       room:addPlayerMark(player, "beilve-turn", 1)
+      end
     end
     if num > 0 then
       player:drawCards(num, beilve.name)
@@ -54,7 +60,8 @@ beilve:addEffect(fk.EventPhaseChanging, {
 
 
 Fk:loadTranslationTable {["pang_beilve"] = "备掠",
-[":pang_beilve"] = "准备阶段，你可以将手牌数摸至三张，然后若你：未因此摸牌且没有护甲，你获得1点护甲；有护甲，你本回合的弃牌阶段改为出牌阶段。",
+[":pang_beilve"] = "准备阶段，你可以将手牌数摸至三张，然后若你：未因此摸牌且没有护甲，你获得1点护甲；有护甲，你可失去1典护甲并将本回合的弃牌阶段改为出牌阶段。",
 ["#beilve"] = "你可以将手牌摸至三张，并根据自身状态获得后续效果",
+["#beilve2"] = "准备完成，开始劫掠！"
 }
 return beilve  --不要忘记返回做好的技能对象哦
