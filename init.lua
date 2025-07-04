96b3ebbb33dc97c6f31894002deb3d0319bfbe67
胖即是胖 <amaipanda@qq.com>
2025-07-04 22:44:22 +0800

    huzhi3

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local panda1 = require "packages/panda/pkg/panda1"
 local panda2 = require "packages/panda/pkg/panda2"
+local panda3 = require "packages/panda/pkg/panda3"
 
 Fk:loadTranslationTable{
   ["panda"] = "胖胖身份",
@@ -7,5 +8,6 @@ Fk:loadTranslationTable{
 
 return{
    panda1,
-   panda2
+   panda2,
+   panda3,
 }
