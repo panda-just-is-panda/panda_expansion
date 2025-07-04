278b0fe76c4f0d212bd735f23ce79c8e231aee34
胖即是胖 <amaipanda@qq.com>
2025-07-04 18:03:51 +0800

    vindicator_remade

--- pkg/panda1/skills/pang_huifu.lua
+++ pkg/panda1/skills/pang_huifu.lua
@@ -2,11 +2,12 @@ local huifu = fk.CreateSkill({
   name = "pang_huifu", ---技能内部名称，要求唯一性
   tags = {}, -- 技能标签，Skill.Compulsory代表锁定技，支持存放多个标签
 })
-huifu:addEffect(fk.EventPhaseStart, { --
+huifu:addEffect(fk.TurnEnd, { --
   anim_type = "drawcard", 
   can_trigger = function(self, event, target, player, data)
-    return target == player and player:hasSkill(self) and
-      player.phase == Player.Finish
+    return target == player and player:hasSkill(self)
+    and #player.room.logic:getActualDamageEvents(1, function(e) return e.data.from == player end) > 0
+    or #player.room.logic:getActualDamageEvents(1, function(e) return e.data.to == player end, Player.HistoryTurn) > 0
   end,
   on_use = function(self, event, target, player, data)
     local room = player.room
@@ -17,7 +18,7 @@ huifu:addEffect(fk.EventPhaseStart, { --
       if player.dead then return false end
     end
     end
-    if #player.room.logic:getActualDamageEvents(1, function(e) return e.data.to == player end, Player.HistoryTurn) == 0 then
+    if #player.room.logic:getActualDamageEvents(1, function(e) return e.data.to == player end, Player.HistoryTurn) > 0 then
          local slash = Fk:cloneCard("slash")
     local max_num = slash.skill:getMaxTargetNum(player, slash)
     local targets = table.filter(room:getOtherPlayers(player, false), function (p)
@@ -43,7 +44,7 @@ end
 })
 
 Fk:loadTranslationTable {["pang_huifu"] = "挥斧",
-[":pang_huifu"] = "结束阶段，若你本回合：造成过伤害，你获得弃牌堆中的一张【杀】；未受到过伤害，你可以视为使用一张【杀】。",
+[":pang_huifu"] = "每回合结束时，若你本回合：造成过伤害，你获得弃牌堆中的一张【杀】；受到过伤害，你可以视为使用一张【杀】。",
 ["#huifu_choose"] = "视为使用一张【杀】"
 }
 return huifu  --不要忘记返回做好的技能对象哦
