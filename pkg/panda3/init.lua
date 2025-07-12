64cf44cba2da627f1348d46fcc0fbba666bd8544
胖即是胖 <amaipanda@qq.com>
2025-07-12 15:55:14 +0800

    历史胖下架

--- pkg/panda3/init.lua
+++ pkg/panda3/init.lua
@@ -8,26 +8,6 @@ Fk:loadTranslationTable{
   ["pang"] = "胖"
 }
 
-local huzhi = General:new(extension, "pang__huzhi", "wei", 3, 3, General.Male)
-huzhi:addSkill("pang_qietian")
-huzhi:addSkill("pang_qieshou")
-Fk:loadTranslationTable{
-["pang__huzhi"] = "胡质",
-["#pang__huzhi"] = "素业贞粹",
-["designer:pang__huzhi"] = "胖即是胖",
-["cv:pang__huzhi"] = "无",
-["illustrator:pang__huzhi"] = "率土之滨",
-}
 
-local fuxuan = General:new(extension, "xiaobai_fuxuan", "jin", 3, 3, General.Male)
-fuxuan:addSkill("xiaobai_shujian")
-fuxuan:addSkill("xiaobai_gangzao")
-Fk:loadTranslationTable{
-["xiaobai_fuxuan"] = "傅玄",
-["#xiaobai_fuxuan"] = "",
-["designer:xiaobai_fuxuan"] = "小汤不圆",
-["cv:xiaobai_fuxuan"] = "无",
-["illustrator:xiaobai_fuxuan"] = "",
-}
 
 return extension
