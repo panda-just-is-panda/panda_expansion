3b67f18dc92e6d8cfdb432b3fee01e730ccb1b8e
胖即是胖 <amaipanda@qq.com>
2025-07-17 02:39:46 +0800

    artanis4

--- pkg/panda2/skills/pang_zhandourongyao.lua
+++ pkg/panda2/skills/pang_zhandourongyao.lua
@@ -3,21 +3,34 @@ local glory = fk.CreateSkill({
   tags = {Skill.Lord}, -- 技能标签，Skill.Compulsory代表锁定技，支持存放多个标签
 })
 
-glory:addEffect(fk.GameStart, {
-    can_refresh = function(self, event, target, player, data)
-        return player:hasSkill(glory.name)
-    end,
-    on_refresh = function(self, event, target, player, data)
-        local room = player.room
-        for _, p in ipairs(room:getAlivePlayers()) do
+glory:addAcquireEffect(function (self, player)
+  local room = player.room
+  for _, p in ipairs(room:getAlivePlayers()) do
             if p.kingdom == "wei" then
                 room:handleAddLoseSkills(p, "pang_jiang", nil, false, true)
+            else
+                room:handleAddLoseSkills(p, "-pang_jiang", nil, false, true)
             end
         end
-    end,
+end)
+
+glory:addEffect(fk.AfterPropertyChange, {
+  can_refresh = function(self, event, target, player, data)
+    return target == player
+  end,
+  on_refresh = function(self, event, target, player, data)
+    local room = player.room
+    if player.kingdom == "wei" and table.find(room.alive_players, function (p)
+      return p:hasSkill(glory.name, true)
+    end) then
+      room:handleAddLoseSkills(player, "pang_jiang", nil, false, true)
+    else
+      room:handleAddLoseSkills(player, "-pang_jiang", nil, false, true)
+    end
+  end,
 })
 
 Fk:loadTranslationTable {["pang_zhandourongyao"] = "战斗荣耀",
-[":pang_zhandourongyao"] = "主公技，游戏开始时，所有魏势力角色获得“激昂”。",
+[":pang_zhandourongyao"] = "主公技，魏势力角色视为拥有“激昂”。",
 }
 return glory
