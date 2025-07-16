94ce1de3dd705e87bec6b2885e49f3bd7e979d72
胖即是胖 <amaipanda@qq.com>
2025-07-16 14:05:39 +0800

    nuodika1

--- pkg/panda2/skills/pang_shengji.lua
+++ pkg/panda2/skills/pang_shengji.lua
@@ -3,8 +3,57 @@ local shengji = fk.CreateSkill {
   tags = {},
 }
 
+shengji:addEffect(fk.EventPhaseStart, {
+anim_type = "offensive",
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(shengji.name) and target.phase == Player.Finish
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local num = 0
+    for _, p in ipairs(room:getAlivePlayers()) do
+        if not p.dead then
+          num = num + p:getLostHp()
+        end
+        end
+    if num > #player:getCardIds("h") then
+    local to_draw = num - #player:getCardIds("h")
+    player:drawCards(to_draw, shengji.name)
+    if #player:getCardIds("h") > 5 then
+      local to_discard = #player:getCardIds("h") - 5
+      local card_discard = room:askToDiscard(player, {
+            skill_name = shengji.name,
+            prompt = "#shengji_discard",
+            cancelable = false,
+            min_num = to_discard,
+            max_num = to_discard,
+            include_equip = false,
+            })
+      local targets = table.filter(room:getOtherPlayers(player, false), function (p)
+      return player:canUseTo("savage_assault", p)
+      end)
+      room:useVirtualCard("savage_assault", nil, player, targets, shengji.name, true)
+    end
+    end
+  end,
+})
+shengji:addEffect(fk.Damage, {
+  can_refresh = function(self, event, target, player, data)
+    return target == player and not data.chain and data.card and table.contains(data.card.skillNames, shengji.name)
+  end,
+  on_refresh = function(self, event, target, player, data)
+      room:recover({
+        who = player,
+        num = 1,
+        recoverBy = player,
+        skillName = shengji.name
+      })
+  end,
+})
+
 Fk:loadTranslationTable {["pang_shengji"] = "生祭",
-[":pang_shengji"] = "结束阶段，你可以将手牌摸至X张（X为所有角色已损失的体力值之和），然后若你的手牌数大于五，你将手牌弃置至五张并视为一张使用【南蛮入侵】；当你因此造成伤害时，你回复1点体力。",
+[":pang_shengji"] = "结束阶段，你可以将手牌摸至X张（X为所有角色已损失的体力值之和），然后若你的手牌数大于五，你将手牌弃置至五张并视为一张使用【南蛮入侵】；当你因此造成伤害后，你回复1点体力。",
+["#shengji_discard"] = "将手牌弃置至5张",
 }
 
 return shengji
--- pkg/panda2/skills/pang_zhiguang.lua
+++ pkg/panda2/skills/pang_zhiguang.lua
@@ -3,8 +3,56 @@ local zhiguang = fk.CreateSkill {
   tags = {},
 }
 
+zhiguang:addEffect("active", {
+  anim_type = "support",
+  card_num = 0,
+  target_num = 0,
+  prompt = "#pang_zhiguang",
+  can_use = function(self, player)
+    return player:usedSkillTimes(zhiguang.name, Player.HistoryPhase) == 0
+  end,
+  on_use = function(self, room, effect)
+    local from = effect.from
+    if not from.dead then
+      room:loseHp(from, 1, zhiguang.name)
+      if not from.dead then
+      local choices = {"zhiguang_kutong", "zhiguang_bihu"}
+      local choice = room:askToChoice(from, {
+      choices = choices,
+      skill_name = zhiguang.name,
+      })
+      if choice == "zhiguang_kutong" then
+        for _, p in ipairs(room:getAlivePlayers()) do
+        if not p.dead then
+          room:loseHp(p, 1, zhiguang.name)
+        end
+        end
+      else
+        local tos = room:askToChoosePlayers(from, {
+        min_num = 1,
+        max_num = 2,
+        targets = room:getAlivePlayers(),
+        skill_name = zhiguang.name,
+        prompt = "#zhiguang_hujia",
+        cancelable = false,
+        })
+        if #tos > 0 then
+          for _, p in ipairs(tos) do
+        room:changeShield(p, 1, {cancelable = false})
+          end
+        end
+      end
+      end
+    end
+  end,
+})
+
 Fk:loadTranslationTable {["pang_zhiguang"] = "织光",
 [":pang_zhiguang"] = "出牌阶段限一次，你可以失去1点体力并选择一项：令所有角色各失去1点体力；令至多两名角色各获得1点护甲。",
+["#pang_zhiguang"] = "你可以失去体力并赐予苦痛或庇护",
+["zhiguang_kutong"] = "令所有角色各失去1点体力",
+["zhiguang_bihu"] = "令至多两名角色各获得1点护甲",
+["#zhiguang_hujia"] = "令至多两名角色各获得1点护甲",
 }
 
 return zhiguang
