6cf7618b2c2ffa7884b1a95c49abebea286e1e78
胖即是胖 <amaipanda@qq.com>
2025-07-18 10:51:41 +0800

    mengsk2

--- pkg/panda2/skills/pang_tiewantongzhi.lua
+++ pkg/panda2/skills/pang_tiewantongzhi.lua
@@ -4,38 +4,173 @@ local tiewan = fk.CreateSkill({
 })
 
 tiewan:addEffect(fk.Damage, {
-  prompt = "#pang_jigao1",
+  mute = true,
   can_trigger = function(self, event, target, player, data)
     return player:hasSkill(tiewan.name) and target == player
   end,
   on_cost = function(self, event, target, player, data)
     local room = player.room
+    local choices = {"wuzhongshengyou", "Cancel"}
+    if player:usedSkillTimes(tiewan.name, Player.HistoryTurn) > 0 then
+      if player:canUseTo(Fk:cloneCard("dismantlement"), data.to) then
+        table.insert(choices, 2, "guohechaiqiao")
+      end
+      local choice = room:askToChoice(player, {
+      choices = choices,
+      skill_name = tiewan.name,
+      prompt = "tiewan_attack2::"..data.to.id,
+      })
+      if choice ~= "Cancel" then
+        event:setCostData(self, {choice = choice})
+        return true
+      end
+    else
+      local targets = table.filter(room:getOtherPlayers(player, false), function (p)
+          return player:canUseTo(Fk:cloneCard("dismantlement"), p)
+          end)
+      if #targets > 0 then
+        table.insert(choices, 2, "guohechaiqiao")
+      end
+      local choice = room:askToChoice(player, {
+      choices = choices,
+      skill_name = tiewan.name,
+      prompt = "tiewan_attack1",
+      })
+      if choice ~= "Cancel" then
+        event:setCostData(self, {choice = choice})
+        return true
+      end
+    end
     end,
     on_use = function(self, event, target, player, data)
-    local room = player.room
+      local room = player.room
+      local choice = event:getCostData(self).choice
+      if player:usedSkillTimes(tiewan.name, Player.HistoryTurn) > 0 then
+        local target = data.to
+        if choice == "wuzhongshengyou" then
+          room:useVirtualCard("ex_nihilo", nil, player, target, tiewan.name, true)
+          player:broadcastSkillInvoke(tiewan.name, 3)
+        elseif choice == "guohechaiqiao" then
+          room:useVirtualCard("dismantlement", nil, player, target, tiewan.name, true)
+          player:broadcastSkillInvoke(tiewan.name, 4)
+        end
+      else
+        if choice == "wuzhongshengyou" then
+          room:useVirtualCard("ex_nihilo", nil, player, player, tiewan.name, true)
+          player:broadcastSkillInvoke(tiewan.name, 1)
+        elseif choice == "wuzhongshengyou" then
+          local targets = table.filter(room:getOtherPlayers(player, false), function (p)
+          return player:canUseTo(Fk:cloneCard("dismantlement"), p)
+          end)
+          if #targets > 0 then
+            local tos = room:askToChoosePlayers(player, {
+            min_num = 1,
+            max_num = 1,
+            targets = targets,
+            skill_name = tiewan.name,
+            prompt = "#tiewan_chai",
+            cancelable = false,
+            })
+            if #tos > 0 then
+              local targets = tos
+              room:sortByAction(targets)
+              room:useVirtualCard("dismantlement", nil, player, targets, tiewan.name, true)
+              player:broadcastSkillInvoke(tiewan.name, 2)
+            end
+          end
+        end
+      end
      end
 })
 
 tiewan:addEffect(fk.Damaged, {
-  prompt = "#pang_jigao1",
+  mute = true,
   can_trigger = function(self, event, target, player, data)
     return player:hasSkill(tiewan.name) and target == player
   end,
   on_cost = function(self, event, target, player, data)
     local room = player.room
+    local choices = {"wuzhongshengyou", "Cancel"}
+    if player:usedSkillTimes(tiewan.name, Player.HistoryTurn) > 0 then
+      if not player:isAllNude() then
+        table.insert(choices, 2, "guohechaiqiao")
+      end
+      local choice = room:askToChoice(player, {
+      choices = choices,
+      skill_name = tiewan.name,
+      prompt = "tiewan_defense2",
+      })
+      if choice ~= "Cancel" then
+        event:setCostData(self, {choice = choice})
+        return true
+      end
+    else
+      local targets = table.filter(room:getOtherPlayers(player, false), function (p)
+          return player:canUseTo(Fk:cloneCard("dismantlement"), p)
+          end)
+      if #targets > 0 then
+        table.insert(choices, 2, "guohechaiqiao")
+      end
+      local choice = room:askToChoice(player, {
+      choices = choices,
+      skill_name = tiewan.name,
+      prompt = "tiewan_defense1",
+      })
+      if choice ~= "Cancel" then
+        event:setCostData(self, {choice = choice})
+        return true
+      end
+    end
     end,
     on_use = function(self, event, target, player, data)
-    local room = player.room
+      local room = player.room
+      local choice = event:getCostData(self).choice
+      if player:usedSkillTimes(tiewan.name, Player.HistoryTurn) > 0 then
+        local target = player
+        if choice == "wuzhongshengyou" then
+          room:useVirtualCard("ex_nihilo", nil, player, target, tiewan.name, true)
+          player:broadcastSkillInvoke(tiewan.name, 7)
+        elseif choice == "guohechaiqiao" then
+          room:useVirtualCard("dismantlement", nil, player, target, tiewan.name, true)
+          player:broadcastSkillInvoke(tiewan.name, 8)
+        end
+      else
+        if choice == "wuzhongshengyou" then
+          room:useVirtualCard("ex_nihilo", nil, player, player, tiewan.name, true)
+          player:broadcastSkillInvoke(tiewan.name, 5)
+        elseif choice == "wuzhongshengyou" then
+          local targets = table.filter(room:getOtherPlayers(player, false), function (p)
+          return player:canUseTo(Fk:cloneCard("dismantlement"), p)
+          end)
+          if #targets > 0 then
+            local tos = room:askToChoosePlayers(player, {
+            min_num = 1,
+            max_num = 1,
+            targets = targets,
+            skill_name = tiewan.name,
+            prompt = "#tiewan_chai",
+            cancelable = false,
+            })
+            if #tos > 0 then
+              local targets = tos
+              room:sortByAction(targets)
+              room:useVirtualCard("dismantlement", nil, player, targets, tiewan.name, true)
+              player:broadcastSkillInvoke(tiewan.name, 6)
+            end
+          end
+        end
+      end
      end
 })
 
 Fk:loadTranslationTable{["pang_tiewantongzhi"] = "铁腕统治",
   [":pang_tiewantongzhi"] = "当你造成或受到伤害后，你可以视为使用一张【无中生有】或【过河拆桥】；若你本回合发动过此技能，此牌的目标角色需为受到伤害的角色。",
-  ["#pang_jigao1"] = "你可以令其摸一张牌并选择失去体力或对自己用【兵粮寸断】",
-  ["losehp"] = "失去体力",
-  ["shortage"] = "获得饥饿",
-  ["use_shortage"] = "将一张黑色牌作为兵粮寸断使用",
-  ["#pang_jigao2"] = "你可以摸一张牌并选择失去体力或对自己用【兵粮寸断】",
+  ["wuzhongshengyou"] = "无中生有",
+  ["guohechaiqiao"] = "过河拆桥",
+  ["tiewan_attack1"] = "你可以视为使用一张无中生有或过河拆桥",
+  ["tiewan_attack2"] = "你可以视为对 %dest 使用一张无中生有或过河拆桥",
+  ["tiewan_defense1"] = "你可以视为使用一张无中生有或过河拆桥",
+  ["tiewan_defense2"] = "你可以视为对自己使用一张无中生有或过河拆桥",
 }
 
 return tiewan
