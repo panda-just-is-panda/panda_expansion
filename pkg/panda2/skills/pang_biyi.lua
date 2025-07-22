5773234f8c3ed29be82b60a889e01c56bb038e44
胖即是胖 <amaipanda@qq.com>
2025-07-22 20:41:23 +0800

    虚构集rework

--- pkg/panda2/skills/pang_biyi.lua
+++ pkg/panda2/skills/pang_biyi.lua
@@ -17,7 +17,7 @@ biyi:addEffect(fk.TargetSpecified, {
 })
 
 biyi:addEffect(fk.EventPhaseEnd, {
-    can_refresh = function(self, event, target, player, data)
+ can_refresh = function(self, event, target, player, data)
     return target == player and player:hasSkill(biyi.name) and player.phase == Player.Play
     end,
     on_refresh = function(self, event, target, player, data)
@@ -26,57 +26,47 @@ biyi:addEffect(fk.EventPhaseEnd, {
       return p:getMark("biyi_used-turn") > 0
     end)
     if #used_tos == #room:getOtherPlayers(player, false) or #used_tos > #room:getOtherPlayers(player, false) then
-      local tos = room:askToChoosePlayers(player, {
+        local tos = room:askToChoosePlayers(player, {
       min_num = 1,
       max_num = 3,
-      targets = room:getOtherPlayers(player, false),
+      targets = used_tos,
       skill_name = biyi.name,
-      prompt = "#biyi1",
+      prompt = "#biyi",
       cancelable = true,
     })
-    if #tos > 0 then
-          for _, p in ipairs(tos) do
-            if not p.dead then
-              room:damage{
-              from = player,
-              to = p,
-              damage = 1,
-              skillName = biyi.name,
-              }
-            end
-          end
-        room:addPlayerMark(player, MarkEnum.AddMaxCards, 1)
+        if #tos > 0 then
+        room:addPlayerMark(player, "biyu_minus-turn", 2)
+        for _, p in ipairs(tos) do
+      if not p.dead then
+        room:damage{
+          from = player,
+          to = p,
+          damage = 1,
+          skillName = biyi.name,
+        }
+      end
+        end
     end
     else
-      local tos = room:askToChoosePlayers(player, {
-      min_num = 1,
-      max_num = 3,
-      targets = room:getOtherPlayers(player, false),
-      skill_name = biyi.name,
-      prompt = "#biyi2",
-      cancelable = true,
-    })
-    if #tos > 0 then
-          for _, p in ipairs(tos) do
-            if not p.dead then
-              room:damage{
-              from = player,
-              to = p,
-              damage = 1,
-              skillName = biyi.name,
-              }
-            end
-          end
-        room:addPlayerMark(player, MarkEnum.MinusMaxCards, 3)
+        room:addPlayerMark(data.to, "biyu_plus-turn", 1)
     end
-  end
+    end,
+})
+biyi:addEffect("maxcards", {
+  correct_func = function(self, player)
+    return -player:getMark("biyu_minus-turn")
+  end,
+})
+biyi:addEffect("maxcards", {
+  correct_func = function(self, player)
+    return player:getMark("biyu_plus-turn")
   end,
 })
 
+
 Fk:loadTranslationTable {["pang_biyi"] = "笔意",
-[":pang_biyi"] = "出牌阶段结束时，你可以对至多三名其他角色各造成1点伤害，然后若你此阶段使用牌指定过所有其他角色为目标，你的手牌上限+1，否则你的手牌上限-3。",
-["#biyi1"] = "你可以对至多三名其他角色各造成1点伤害并令手牌上限+1",
-["#biyi2"] = "你可以对至多三名其他角色各造成1点伤害并令手牌上限-3",
+[":pang_biyi"] = "出牌阶段结束时，若你此阶段使用牌指定过所有其他角色为目标，你可以对至多三名其他角色各造成1点伤害且本回合你的手牌上限-2，否则本回合你的手牌上限+1。",
+["#biyi"] = "你可以对至多三名其他角色各造成1点伤害并令手牌上限本回合-2",
 }
 
 return biyi
