eb8fbb7ee4b74a93cb9fcdd36da1cd83e8b9c67d
胖即是胖 <amaipanda@qq.com>
2025-07-31 03:09:29 +0800

    胖胖7

--- pkg/panda1/skills/pang_taipang.lua
+++ pkg/panda1/skills/pang_taipang.lua
@@ -7,28 +7,37 @@ taipang:addEffect(fk.TargetSpecifying, {
   anim_type = "offensive",
    can_trigger = function(self, event, target, player, data)
     return target == player and player:hasSkill(taipang.name) and table.contains(data.use.tos, player)
+    and (player:getMark("taipang1-round") == 0 or player:getMark("taipang2-round") == 0)
   end,
   on_use = function(self, event, target, player, data)
     local room = player.room
     data:cancelTarget(player)
-    local choices = {"#pang_taipang1", "#pang_taipang2"}
+    local choices = {}
+    if player:getMark("taipang1-round") == 0 then
+        table.insert(choices, 1, "#pang_taipang1")
+    end
+    if player:getMark("taipang2-round") == 0 then
+        table.insert(choices, 2, "#pang_taipang2")
+    end
     local choice = room:askToChoice(player, {
       choices = choices,
       skill_name = taipang.name,
     })
     if choice == "#pang_taipang1" then
+        room:addPlayerMark(target, "taipang1-round", 1)
         local to_draw = player:getMaxCards() - player:getHandcardNum()
         if to_draw > 0 then
              player:drawCards(to_draw, taipang.name)
         end
     else
+        room:addPlayerMark(target, "taipang2-round", 1)
         room:addPlayerMark(player, MarkEnum.AddMaxCards, 1)
     end
   end,
 })
 
 Fk:loadTranslationTable {["pang_taipang"] = "太胖",
-[":pang_taipang"] = "当你使用牌指定自己为目标时，你可以取消之并选择一项：将手牌摸至手牌上限；令你的手牌上限+1。",
+[":pang_taipang"] = "每轮各限一次，当你使用牌指定自己为目标时，你可以取消之并选择一项：将手牌摸至手牌上限；令你的手牌上限+1。",
 ["#pang_taipang1"] = "将手牌摸至手牌上限",
 ["#pang_taipang2"] = "令你的手牌上限+1",
 }
