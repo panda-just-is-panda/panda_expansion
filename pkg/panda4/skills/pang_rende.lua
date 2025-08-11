1235a50490171c64e316bf892b4c54b48136a61a
胖即是胖 <amaipanda@qq.com>
2025-08-11 22:54:50 +0800

    仁德prototype

---
+++ pkg/panda4/skills/pang_rende.lua
@@ -0,0 +1,84 @@
+local U = require "packages/utility/utility"
+
+Fk:loadTranslationTable{
+  ["pang_rende"] = "仁德",
+  [":pang_rende"] = "出牌阶段每名角色限一次，你可以将任意张手牌交给一名其他角色，然后其可以交给你至多等量张手牌；每阶段你以此法给出第二张牌时，你可以令一名角色视为使用一张基本牌。",
+  ["#pang_rende"] = "仁德：将任意张手牌交给一名角色，若此阶段交出达到两张，你可以视为使用一张基本牌",
+
+  ["#pang_rende-ask"] = "仁德：你可视为使用一张基本牌",
+  ["#pang_rende-return"] = "仁德：你可以交给刘备一张手牌",
+  ["#rende-choose"] = "仁德：令一名角色可以视为使用一张基本牌",
+
+  ["$pang_rende1"] = "同心同德，救困扶危！",
+  ["$pang_rende2"] = "施仁布泽，乃我大汉立国之本！",
+}
+
+local rende = fk.CreateSkill{
+  name = "pang_rende",
+}
+
+rende:addEffect("active", {
+  anim_type = "support",
+  min_card_num = 1,
+  target_num = 1,
+  prompt = "#pang_rende",
+  card_filter = function(self, player, to_select, selected)
+    return table.contains(player:getCardIds("h"), to_select)
+  end,
+  target_filter = function(self, player, to_select, selected)
+    return #selected == 0 and to_select ~= player and not table.contains(player:getTableMark("ex__rende-phase"), to_select.id)
+  end,
+  on_use = function(self, room, effect)
+    local player = effect.from
+    local target = effect.tos[1]
+    local cards = effect.cards
+    room:addTableMark(player, "ex__rende-phase", target.id)
+    local n = player:getMark("ex__rende_num-phase")
+    room:moveCardTo(cards, Player.Hand, target, fk.ReasonGive, rende.name, nil, false, player)
+    room:addPlayerMark(player, "ex__rende_num-phase", #cards)
+    local max_num = #cards
+    local card_return = room:askToCards(target, {
+        min_num = max_num,
+        max_num = max_num,
+        include_equip = false,
+        skill_name = rende.name,
+        prompt = "#pang_rende-return",
+        cancelable = true,
+      })
+    room:obtainCard(player, card_return, false, fk.ReasonGive)
+    if n < 2 and n + #cards >= 2 then
+      cards = U.getUniversalCards(room, "b")
+      local tos = room:askToChoosePlayers(player, {
+      min_num = 1,
+      max_num = 1,
+      targets = room:getOtherPlayers(player, false),
+      skill_name = rende.name,
+      prompt = "#rende-choose",
+      cancelable = true,
+    })
+      local use = room:askToUseRealCard(tos, {
+        pattern = cards,
+        skill_name = rende.name,
+        prompt = "#pang_rende-ask",
+        extra_data = {
+          bypass_times = false,
+          extraUse = false,
+          expand_pile = cards,
+        },
+        cancelable = true,
+        skip = true,
+      })
+      if use then
+        local card = Fk:cloneCard(use.card.name)
+        card.skillName = rende.name
+        room:useCard{
+          from = player,
+          tos = use.tos,
+          card = card,
+        }
+      end
+    end
+  end,
+})
+
+return rende
