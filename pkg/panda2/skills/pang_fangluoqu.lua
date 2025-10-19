57afd59b6ac5c2ae4ceed7392e93a504f0c8e6ff
胖即是胖 <15991111+panda_is_panda@user.noreply.gitee.com>
2025-10-18 22:23:19 -0700

    mothersdilk2

--- pkg/panda2/skills/pang_fangluoqu.lua
+++ pkg/panda2/skills/pang_fangluoqu.lua
@@ -4,6 +4,6 @@ local jjf = fk.CreateSkill {
 }
 
 Fk:loadTranslationTable{
-  ["pang_jianjiefu"] = "纺络曲",
-  [":pang_jianjiefu"] = "主公技，觉醒技，准备阶段，若所有忠臣和内奸均已死亡，“茧结缚”失效直到你于一回合内使用两张牌名相同的牌。",
+  ["pang_fangluoqu"] = "纺络曲",
+  [":pang_fangluoqu"] = "主公技，觉醒技，准备阶段，若所有忠臣和内奸均已死亡，“茧结缚”失效直到你于一回合内使用两张牌名相同的牌。",
 }
