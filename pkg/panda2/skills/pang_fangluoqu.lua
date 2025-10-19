e18f9c14b8f664c6eed213a596e209c0f8ac8013
胖即是胖 <15991111+panda_is_panda@user.noreply.gitee.com>
2025-10-19 09:03:01 -0700

    丝母2

--- pkg/panda2/skills/pang_fangluoqu.lua
+++ pkg/panda2/skills/pang_fangluoqu.lua
@@ -8,4 +8,41 @@ Fk:loadTranslationTable{
   [":pang_fangluoqu"] = "主公技，觉醒技，准备阶段，若所有忠臣和内奸均已死亡，“茧结缚”失效直到你于一回合内使用两张牌名相同的牌。",
 }
 
+flq:addEffect(fk.EventPhaseStart, {
+  can_trigger = function(self, event, target, player, data)
+    local melody = table.filter(room:getOtherPlayers(player, false), function (p)
+      return p.role ~= "rebel"
+    end)
+    return target == player and player:hasSkill(flq.name) and
+      player.phase == Player.Start and
+      #melody == 0 and
+      player:usedSkillTimes(flq.name, Player.HistoryGame) == 0
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    room:setPlayerMark(player, "flq_awake", 1)
+  end,
+})
+
+flq:addEffect("invalidity", {
+  invalidity_func = function(self, from, skill)
+    return
+      from:getMark("flq_awake") > 0 and skill:isPlayerSkill(from, flq.name, true)
+  end
+})
+
+flq:addEffect(fk.Cardusing, {
+  anim_type = "negative",
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(flq.name) and
+      #player.room.logic:getEventsOfScope(GameEvent.UseCard, 2, function(e)
+        local use = e.data
+        return use.card.trueName == data.card.trueName and table.contains(use.from, player)
+      end, Player.HistoryTurn) > 1
+  end,
+  on_use = function(self, event, target, player, data)
+    player.room:setPlayerMark(player, "flq_awake", 0)
+  end,
+})
+
 return flq
--- pkg/panda2/skills/pang_siweijiang.lua
+++ pkg/panda2/skills/pang_siweijiang.lua
@@ -5,7 +5,110 @@ local swj = fk.CreateSkill {
 
 Fk:loadTranslationTable{
   ["pang_siweijiang"] = "丝为缰",
-  [":pang_siweijiang"] = "当点数为4的牌进入弃牌堆时，你获得之；每回合开始时，你可以将一张点数为4的牌置于牌堆顶；当其他角色失去点数为4的牌时，你可以获得其两张牌。",
+  [":pang_siweijiang"] = "当点数为4的牌进入弃牌堆时，你获得之；每回合开始时，你可以将一张点数为4的牌置于牌堆顶；当其他角色使用点数为4的牌时，你可以获得其两张牌。",
+  ["#swj"] = "丝为缰：你可以将一张点数为4的牌置于牌堆顶",
+  ["#swj_tou"] = "丝为缰：你可以获得%src两张牌"
 }
+
+swj:addEffect(fk.AfterCardsMove, {
+  anim_type = "drawcard",
+  can_trigger = function(self, event, target, player, data)
+    if player:hasSkill(swj.name) then
+      local ids = {}
+      for _, move in ipairs(data) do
+        if move.toArea == Card.DiscardPile then
+            for _, info in ipairs(move.moveInfo) do
+              if Fk:getCardById(info.cardId).number == 4 then
+                table.insertIfNeed(ids, info.cardId)
+              end
+            end
+          end
+        end
+      ids = table.filter(ids, function (id)
+        return table.contains(player.room.discard_pile, id)
+      end)
+      ids = player.room.logic:moveCardsHoldingAreaCheck(ids)
+      if #ids > 0 then
+        event:setCostData(self, {cards = ids})
+        return true
+      end
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local ids = table.simpleClone(event:getCostData(self).cards)
+    room:moveCardTo(ids, Card.PlayerHand, player, fk.ReasonJustMove, swj.name, nil, true, player)
+  end,
+})
+
+swj:addEffect(fk.TurnStart, {
+  anim_type = "control",
+  can_trigger = function(self, event, target, player, data)
+    local card = table.filter(player:getCardIds("he"), function(id)
+        local card_pick = Fk:getCardById(id)
+        if card_pick.number == 4 then
+          return card_pick
+        end
+        end)
+    if player:hasSkill(swj.name)
+    and #card > 0 then
+        return true
+    end
+  end,
+  on_use = function (self, event, target, player, data)
+    local room = player.room
+    local card = table.filter(player:getCardIds("he"), function(id)
+        local card_pick = Fk:getCardById(id)
+        if card_pick.number == 4 then
+          return card_pick
+        end
+        end)
+    local to_select = room:askToCards(player, {
+      min_num = 1,
+      max_num = 1,
+      include_equip = true,
+      pattern = ".|4",
+      skill_name = swj.name,
+      prompt = "#swj",
+      cancelable = false,
+    })
+        if #to_select > 0 then
+        room:moveCards({
+      ids = to_select,
+      from = player,
+      toArea = Card.DrawPile,
+      moveReason = fk.ReasonPut,
+      skillName = swj.name,
+      proposer = player,
+      moveVisible = true,
+    })
+        end
+  end,
+})
+
+swj:addEffect(fk.CardUsing, {
+  anim_type = "control",
+  can_trigger = function(self, event, target, player, data)
+    return target ~= player and not target:isNude() and player:hasSkill(swj.name) and data.card.number == 4
+  end,
+  on_cost = function(self, event, target, player, data)
+    return player.room:askToSkillInvoke(target, {
+      skill_name = swj.name,
+      prompt = "#swj_tou:"..target.id,
+    })
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local cards = room:askToChooseCards(player, {
+      target = target,
+      min = 2,
+      max = 2,
+      flag = "he",
+      skill_name = swj.name,
+    })
+    room:obtainCard(player, cards, false, fk.ReasonPrey, player, swj.name)
+  end,
+})
+
 
 return swj
