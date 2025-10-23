1f2e67ccf153ceb9d6b2067dc1bd36b05af5febf
胖即是胖 <15991111+panda_is_panda@user.noreply.gitee.com>
2025-10-22 22:31:49 -0700

    last_judge2

--- pkg/panda2/skills/pang_zhongcai.lua
+++ pkg/panda2/skills/pang_zhongcai.lua
@@ -2,12 +2,11 @@ local zhongcai = fk.CreateSkill {
   name = "pang_zhongcai",
 }
 
-Fk:loadTranslationTable{
+Fk:loadTranslationTable {
   ["pang_zhongcai"] = "忠裁",
   [":pang_zhongcai"] = "锁定技，游戏开始时，你获得一张【杀】；每回合结束时，你获得此【杀】；你除此【杀】之外的【杀】视为【铁索连环】。",
 
   ["@@pang_zhongcai-inhand"] = "忠裁",
-  ["@tyj__weiming-inhand"] = "忠裁",
 
 }
 
