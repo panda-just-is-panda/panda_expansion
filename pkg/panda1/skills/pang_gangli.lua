c122a2c955735eea2fd2ce7d1f315dccb17f14a8
胖即是胖 <15991111+panda_is_panda@user.noreply.gitee.com>
2025-12-03 19:48:04 -0800

    irongolem8

--- pkg/panda1/skills/pang_gangli.lua
+++ pkg/panda1/skills/pang_gangli.lua
@@ -39,9 +39,8 @@ gangli:addEffect(fk.PreCardUse, {
 
 gangli:addEffect("targetmod", {
   bypass_times = function(self, player, skill, scope, card, to)
-    return card and to and to:hasSkill(gangli.name)
-    and card.trueName == "slash" 
-    and to:getSwitchSkillState(gangli.name, true) ~= fk.SwitchYang
+    return card and to and to:hasSkill(gangli.name) and to:getSwitchSkillState(gangli.name) ~= fk.SwitchYang
+    and card.trueName == "slash" and to ~= player
   end,
 })
 
