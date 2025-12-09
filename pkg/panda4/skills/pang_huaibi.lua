a0c1e6112e912fbbfc7eda48984135ccb8c274c3
胖即是胖 <15991111+panda_is_panda@user.noreply.gitee.com>
2025-12-08 17:03:05 -0800

    刘璋改

--- pkg/panda4/skills/pang_huaibi.lua
+++ pkg/panda4/skills/pang_huaibi.lua
@@ -5,7 +5,7 @@ local huaibi = fk.CreateSkill {
 
 Fk:loadTranslationTable{
   ["pang_huaibi"] = "怀璧",
-  [":pang_huaibi"] = "主公技，锁定技，游戏开始时，你获得2点护甲；有护甲的群势力角色受到伤害时，伤害来源获得1点护甲。",
+  [":pang_huaibi"] = "主公技，锁定技，游戏开始时，你获得X点护甲(X为群势力角色数)；有护甲的你受到伤害时，伤害来源获得1点护甲。",
 
   ["$pang_huaibi1"] = "哎！匹夫无罪，怀璧其罪啊。",
   ["$pang_huaibi2"] = "粮草尽皆在此，宗兄可自取之。",
@@ -19,14 +19,20 @@ huaibi:addEffect(fk.GameStart, {
   on_cost = Util.TrueFunc,
   on_use = function(self, event, target, player, data)
     local room = player.room
-    room:changeShield(player, 2, {cancelable = false})
+    local X = 0
+    for _, p in ipairs(Fk:currentRoom().alive_players) do
+      if p.kingdom == "qun" then
+        X = X + 1
+      end
+    end
+    room:changeShield(player, X, {cancelable = false})
   end,
 })
 
 huaibi:addEffect(fk.DamageInflicted, {
   anim_type = "negative",
   can_trigger = function(self, event, target, player, data)
-    return player:hasSkill(huaibi.name) and target.kingdom == "qun" and target.shield > 0 and data.from
+    return player:hasSkill(huaibi.name) and target == player and target.shield > 0 and data.from
   end,
   on_cost = Util.TrueFunc,
   on_use = function(self, event, target, player, data)
