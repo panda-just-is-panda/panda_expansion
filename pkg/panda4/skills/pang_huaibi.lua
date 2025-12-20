97ca5f2ecb7efbc96c928ffcd0f7a2bc5a11d4a1
胖即是胖 <15991111+panda_is_panda@user.noreply.gitee.com>
2025-12-20 17:04:15 +0800

    成就？

--- pkg/panda4/skills/pang_huaibi.lua
+++ pkg/panda4/skills/pang_huaibi.lua
@@ -3,6 +3,7 @@ local huaibi = fk.CreateSkill {
   tags = { Skill.Compulsory, Skill.Lord },
 }
 
+local U = require "packages.utility.utility"
 Fk:loadTranslationTable{
   ["pang_huaibi"] = "怀璧",
   [":pang_huaibi"] = "主公技，锁定技，游戏开始时，你获得X点护甲(X为群势力角色数)；有护甲的你受到伤害时，伤害来源获得1点护甲。",
@@ -10,6 +11,13 @@ Fk:loadTranslationTable{
   ["$pang_huaibi1"] = "哎！匹夫无罪，怀璧其罪啊。",
   ["$pang_huaibi2"] = "粮草尽皆在此，宗兄可自取之。",
 }
+local gdU
+if Fk.skills["glory_days__show"] then
+    gdU = require "packages/glory_days/utility"
+    if type(gdU.RegisterAchievement) == "function" then
+      gdU.RegisterAchievement("胖胖胖胖","怀璧其罪","谁家董卓？","游戏开始时因“怀璧”获得5点护甲","general:pang__liuzhang",true,nil,true)
+    end
+end
 
 huaibi:addEffect(fk.GameStart, {
   anim_type = "defensive",
@@ -26,6 +34,12 @@ huaibi:addEffect(fk.GameStart, {
       end
     end
     room:changeShield(player, X, {cancelable = false})
+    if X > 4 then
+      if Fk.skills["glory_days__show"] and gdU and player:getMark(huaibi.name.."_achive")==0 then
+        room:setPlayerMark(player,huaibi.name.."_achive",1)
+        gdU.addAchievement(room,"steam",250,nil,"怀璧其罪","谁家董卓？","游戏开始时因“怀璧”获得5点护甲","general:pang__liuzhang", {player})
+      end
+    end
   end,
 })
 
