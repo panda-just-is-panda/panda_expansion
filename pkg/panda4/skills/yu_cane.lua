1f5c54e82ac192e97b10732c5943c96683154e32
胖即是胖 <15991111+panda_is_panda@user.noreply.gitee.com>
2026-01-10 21:17:31 -0800

    徐荣6

--- pkg/panda4/skills/yu_cane.lua
+++ pkg/panda4/skills/yu_cane.lua
@@ -5,7 +5,7 @@ tags = { Skill.Compulsory },
 
 Fk:loadTranslationTable{
   ["yu_cane"] = "残厄",
-  [":yu_cane"] = "锁定技，你造成/受到伤害时，摸两张牌并将本技能交给上家/下家：本技能每来回途经一名角色后,“神徐荣”对其造成1点火焰伤害。",
+  [":yu_cane"] = "锁定技，你造成/受到伤害时，摸两张牌并将本技能交给上家/下家：本技能每来回途经一名角色后,其失去1点体力。",
 
   ["@@cane_gain"] = "残厄 获得过",
   ["@@cane_lost"] = "残厄 失去过",
@@ -26,21 +26,9 @@ anim_type = "drawcard",
     room:addPlayerMark(to, "@@cane_gain")
     for _, q in ipairs(room:getAlivePlayers()) do
         if q:getMark("@@cane_lost") > 0 and q:getMark("@@cane_gain") > 0 then
-            for _, p in ipairs(room.players) do
-                if p.general == "yu_xurong" or p.deputyGeneral == "yu_xurong" then
-                    room:damage{
-                        from = p,
-                        to = q,
-                        damage = 1,
-                        damageType = fk.FireDamage,
-                        skillName = cane.name,
-                    }
-                    if not q.dead then 
-                        room:setPlayerMark(q, "@@cane_gain", 0)
-                        room:setPlayerMark(q, "@@cane_lost", 0)
-                    end
-                end
-            end
+            room:loseHp(q, 1, cane.name)
+            room:setPlayerMark(q, "@@cane_gain", 0)
+            room:setPlayerMark(q, "@@cane_lost", 0)
         end
     end
   end,
@@ -62,21 +50,9 @@ anim_type = "drawcard",
     room:addPlayerMark(to, "@@cane_gain")
     for _, q in ipairs(room:getAlivePlayers()) do
         if q:getMark("@@cane_lost") > 0 and q:getMark("@@cane_gain") > 0 then
-            for _, p in ipairs(room.players) do
-                if p.general == "yu_xurong" or p.deputyGeneral == "yu_xurong" then
-                    room:damage{
-                        from = p,
-                        to = q,
-                        damage = 1,
-                        damageType = fk.FireDamage,
-                        skillName = cane.name,
-                    }
-                    if not q.dead then 
-                        room:setPlayerMark(q, "@@cane_gain", 0)
-                        room:setPlayerMark(q, "@@cane_lost", 0)
-                    end
-                end
-            end
+            room:loseHp(q, 1, cane.name)
+            room:setPlayerMark(q, "@@cane_gain", 0)
+            room:setPlayerMark(q, "@@cane_lost", 0)
         end
     end
   end,
