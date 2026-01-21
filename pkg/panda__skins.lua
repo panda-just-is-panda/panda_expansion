2dc59c3d9ccbb63db0a0e0ade6f74104db301f59
胖即是胖 <amaipanda@qq.com>
2026-01-21 00:19:26 -0800

    括号胖

--- pkg/panda__skins.lua
+++ pkg/panda__skins.lua
@@ -4,9 +4,8 @@ extension.extensionName = "panda__skins"
 extension:addSkinPackage {
   path = "packages/panda/image/generals",
   content = {
-skins={"pang__pangpanda.jpg"},
-enable_generals = {"pang__panda"},
-  }
+  {skins={"pang__pangpanda.jpg"}, enable_generals = {"pang__panda"},}
+}
 }
 
 return extension
