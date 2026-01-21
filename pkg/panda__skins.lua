8be2144fed9bad831eb5eac178b58fccd0bcdcd6
胖即是胖 <amaipanda@qq.com>
2026-01-21 00:52:00 -0800

    太胖太胖

--- pkg/panda__skins.lua
+++ pkg/panda__skins.lua
@@ -6,6 +6,22 @@ local content1 = {
   skins={"pang__classicpanda.jpg"}, 
   enabled_generals = {"pang__panda"},
   },
+  {
+  skins={"pang__speechlesspanda.jpg"}, 
+  enabled_generals = {"pang__panda"},
+  },
+  {
+  skins={"pang__flowerpanda.jpg"}, 
+  enabled_generals = {"pang__panda"},
+  },
+  {
+  skins={"pang__tangpanda.jpg"}, 
+  enabled_generals = {"pang__panda"},
+  },
+  {
+  skins={"pang__moviepanda.jpg"}, 
+  enabled_generals = {"pang__panda"},
+  },
 }
 extension:addSkinPackage {
   path = "/image/skins",
@@ -13,6 +29,10 @@ extension:addSkinPackage {
 }
 Fk:loadTranslationTable{
   ["pang__classicpanda"] = "经典胖胖",
+  ["pang__speechlesspanda"] = "无语胖胖",
+  ["pang__flowerpanda"] = "送花胖胖",
+  ["pang__tangpanda"] = "唐胖",
+  ["pang__moviepanda"] = "大电影胖胖",
 }
 
 return extension
