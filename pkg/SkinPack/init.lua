b4f397239ecca6c7de1a8610ad815e46e174fcac
胖即是胖 <amaipanda@qq.com>
2026-01-20 23:48:24 -0800

    皮肤胖

---
+++ pkg/SkinPack/init.lua
@@ -0,0 +1,12 @@
+local extension = Package:new("panda__skins", Package.SkinPack)
+extension.extensionName = "panda__skin1"
+
+extension:addSkinPackage {
+  path = "packages/panda/image/generals",
+  content = {
+skins={"pang__pangpanda.jpg"},
+enable_generals = {"pang__panda"},
+  }
+}
+
+return extension
