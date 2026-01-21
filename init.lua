f174a58912465e0417d74b6421e6f85238f7f9e2
胖即是胖 <amaipanda@qq.com>
2026-01-21 00:17:01 -0800

    皮肤胖6

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-local panda1 = require "packages/panda/pkg/panda1"
-local panda2 = require "packages/panda/pkg/panda2"
-local panda3 = require "packages/panda/pkg/panda3"
-local panda4 = require "packages/panda/pkg/panda4"
-local panda_skin = require "packages/panda/pkg/panda__skins"
+local panda1 = require "packages.panda.pkg.panda1"
+local panda2 = require "packages.panda.pkg.panda2"
+local panda3 = require "packages.panda.pkg.panda3"
+local panda4 = require "packages.panda.pkg.panda4"
+local panda_skin = require "packages.panda.pkg.panda__skins"
 
 Fk:loadTranslationTable{
   ["panda"] = "胖胖身份",
