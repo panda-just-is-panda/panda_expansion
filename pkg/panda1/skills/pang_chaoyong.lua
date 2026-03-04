5844e737d950190e258e88cb2dbb6970decf3f71
胖即是胖 <amaipanda@qq.com>
2026-03-04 01:52:14 -0800

    溺尸代码优化

--- pkg/panda1/skills/pang_chaoyong.lua
+++ pkg/panda1/skills/pang_chaoyong.lua
@@ -13,15 +13,25 @@ chaoyong:addEffect(fk.CardUsing, {
   on_use = function(self, event, target, player, data)
     local room = player.room
     local use_events1 = player.room.logic:getEventsOfScope(GameEvent.UseCard, 1, function (e)
-        local use1 = e.data
-        return use1.from == player and use1.card.type == data.card.type
-      end, Player.HistoryTurn)
-      local use_events2 = player.room.logic:getEventsOfScope(GameEvent.UseCard, 1, function (e)
-        local use2 = e.data
-        return use2.from == player and use2.card.suit == data.card.suit
-      end, Player.HistoryTurn)
+      local use1 = e.data
+      return use1.from == player and use1.card.type == data.card.type
+    end, Player.HistoryTurn)
+    local use_events2 = player.room.logic:getEventsOfScope(GameEvent.UseCard, 1, function (e)
+      local use2 = e.data
+      return use2.from == player and use2.card.suit == data.card.suit
+    end, Player.HistoryTurn)
     if #use_events1 == 1 and use_events1[1].data == data or #use_events2 == 1 and use_events2[1].data == data then
          player:drawCards(1, chaoyong.name)
+         if not table.contains(player:getTableMark("@chaoyong_suit-turn"), "log_" .. data.card:getSuitString()) then
+            local typesRecorded = player:getTableMark("@chaoyong_suit-turn")
+            table.insert(typesRecorded, "log_" .. data.card:getSuitString())
+            room:setPlayerMark(player, "@chaoyong_suit-turn", typesRecorded)
+         end
+         if not table.contains(player:getTableMark("@chaoyong_type-turn"), "log_" .. data.card:getTypeString()) then
+            local typesRecorded = player:getTableMark("@chaoyong_type-turn")
+            table.insert(typesRecorded, "log_" .. data.card:getTypeString())
+            room:setPlayerMark(player, "@chaoyong_type-turn", typesRecorded)
+         end
     else room:loseHp(player, 1, chaoyong.name)
   end
 end
